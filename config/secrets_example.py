7ce5b330574952fc4158385f8c558dec5a29cf63
Abdel505 <abdelalouan07@gmail.com>
2026-01-03 13:24:50 +0100

    Mqtt certificate and credentials

--- config/secrets_example.py
+++ config/secrets_example.py
@@ -1,8 +1,18 @@
 # Rename this file to secrets.py and fill in your details
 
-WIFI_SSID = "YOUR_WIFI_NAME_HERE"
-WIFI_PASS = "YOUR_WIFI_PASSWORD_HERE"
-MQTT_SERVER = "192.168.1.XXX"
-MQTT_PORT = 1883
+# WiFi Credentials
+WIFI_SSID = "YOUR_WIFI_SSID"
+WIFI_PASS = "YOUR_WIFI_PASSWORD"
 
-SETUP_PASSWORD = "admin" # Password for Setup Mode
+# MQTT Broker Configuration
+MQTT_SERVER = "your-cluster-url.s1.eu.hivemq.cloud"
+MQTT_PORT = 8883
+MQTT_SSL = True
+
+# MQTT Certificate & Credentials
+MQTT_CERT = 'hivemq.pem'
+MQTT_USER = "YOUR_MQTT_USERNAME"
+MQTT_PASS = "YOUR_MQTT_PASSWORD"
+
+# Device Security
+SETUP_PASSWORD = "SET_UP_PASSWORD" # Password for Setup Mode
